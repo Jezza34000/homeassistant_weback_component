d2248ef55c6231e88f8f355b3be4f0e9d8ffbaf6
jalcaras <jalcaras@free-mobile.Fr>
2023-02-13 14:47:39 +0100

    refactor (vacdevice) code improvement : isort, black, flake8, pylint

--- custom_components/weback_vacuum/vacdevice.py
+++ custom_components/weback_vacuum/vacdevice.py
@@ -1,15 +1,35 @@
-import logging
-import io
+"""
+VacDevice Module
+"""
 
-from .WebackApi import WebackWssCtrl
-from .VacMap import VacMap, VacMapDraw, VacMapRoom
+import io
+import logging
+
+from .vacmap import VacMap, VacMapDraw, VacMapRoom
+from .webackapi import WebackWssCtrl
 
 _LOGGER = logging.getLogger(__name__)
 
 
 class VacDevice(WebackWssCtrl):
-    def __init__(self, thing_name, thing_nickname, sub_type, thing_status,
-                 user, password, region, country, app, client_id, api_version):
+    """
+    VacDevice Class
+    """
+
+    def __init__(
+        self,
+        thing_name,
+        thing_nickname,
+        sub_type,
+        thing_status,
+        user,
+        password,
+        region,
+        country,
+        app,
+        client_id,
+        api_version,
+    ):
         _LOGGER.debug("WebackApi RobotController __init__")
         super().__init__(user, password, region, country, app, client_id, api_version)
         self.name = thing_name
@@ -27,12 +47,14 @@ class VacDevice(WebackWssCtrl):
     # Update controller
 
     async def watch_state(self):
-        """ State watcher from VacDevice"""
-        _LOGGER.debug(f"VacDevice: starting state watcher for= {self.name} {self.sub_type}")
+        """State watcher from VacDevice"""
+        _LOGGER.debug(
+            "VacDevice: starting state watcher for= %s (%s)", self.name, self.sub_type
+        )
         try:
             await self.refresh_handler(self.name, self.sub_type)
         except:
-            _LOGGER.exception('Error on watch_state starting refresh_handler')
+            _LOGGER.exception("Error on watch_state starting refresh_handler")
 
     async def load_maps(self):
         """Load the current reuse map"""
@@ -40,12 +62,15 @@ class VacDevice(WebackWssCtrl):
         if self.ACTIVE_MAP_ID_PROP not in self.robot_status:
             return False
 
-        map_data = await self.get_reuse_map_by_id(self.robot_status[self.ACTIVE_MAP_ID_PROP], self.sub_type, self.name)
-        if map_data is not []:
+        map_data = await self.get_reuse_map_by_id(
+            self.robot_status[self.ACTIVE_MAP_ID_PROP], self.sub_type, self.name
+        )
+        if map_data:
             self.map = VacMap(map_data)
             self.render_map()
 
     def render_map(self):
+        """ Rendering Map """
         if not self.map:
             return False
 
@@ -57,7 +82,7 @@ class VacDevice(WebackWssCtrl):
         img = vac_map_draw.get_image()
 
         img_byte_arr = io.BytesIO()
-        img.save(img_byte_arr, format='PNG')
+        img.save(img_byte_arr, format="PNG")
         img.close()
         self.map_image_buffer = img_byte_arr.getvalue()
 
@@ -68,100 +93,109 @@ class VacDevice(WebackWssCtrl):
         return True
 
     def register_map_camera(self, camera):
+        """ Register map camera """
         self.map_camera = camera
 
     def trigger_map_camera_update(self):
+        """ Trigger map camera update """
         if self.map_camera is not None:
             self.map_camera.schedule_update_ha_state(True)
 
     # ==========================================================
     # Vacuum Entity
     # -> Properties
-    
+
     @property
     def current_mode(self):
-        """ Raw working_status field string """
-        if 'working_status' in self.robot_status:
-            return self.robot_status['working_status']
+        """Raw working_status field string"""
+        if "working_status" in self.robot_status:
+            return self.robot_status["working_status"]
         return self.IDLE_MODE
-    
+
     @property
     def raw_status(self) -> str:
-        """ Raw thing_status JSON """
-        _LOGGER.debug(f"raw_status {self.robot_status}")
+        """Raw thing_status JSON"""
+        _LOGGER.debug("raw_status %s", self.robot_status)
         return self.robot_status
-    
+
     @property
     def is_cleaning(self) -> bool:
-        """ Boolean define if robot is in cleaning state """
-        _LOGGER.debug(f"is_cleaning = {self.current_mode}")
+        """Boolean define if robot is in cleaning state"""
+        _LOGGER.debug("is_cleaning = %s ", self.current_mode)
         return self.current_mode in self.CLEANING_STATES
-    
+
     @property
     def is_available(self):
-        """ Boolean define if robot is connected to cloud """
-        if 'connected' in self.robot_status:
-            return self.robot_status['connected'] == 'true'
+        """Boolean define if robot is connected to cloud"""
+        if "connected" in self.robot_status:
+            return self.robot_status["connected"] == "true"
         return False
-    
+
     @property
     def is_charging(self):
-        """ Boolean define if robot is charging """
+        """Boolean define if robot is charging"""
         return self.current_mode in self.CHARGING_STATES
-    
+
     @property
     def error_info(self):
-        """ Raw error_info field string """
-        if 'error_info' in self.robot_status:
-            return self.robot_status['error_info']
+        """Raw error_info field string"""
+        if "error_info" in self.robot_status:
+            return self.robot_status["error_info"]
         return None
-    
+
     @property
     def battery_level(self):
-        """ Raw battery_level field integer """
-        if 'battery_level' in self.robot_status:
-            return int(self.robot_status['battery_level'])
+        """Raw battery_level field integer"""
+        if "battery_level" in self.robot_status:
+            return int(self.robot_status["battery_level"])
         return 0
 
     @property
     def fan_status(self):
-        """ Raw fan_status field string """
-        if 'fan_status' in self.robot_status:
-            return self.robot_status['fan_status']
+        """Raw fan_status field string"""
+        if "fan_status" in self.robot_status:
+            return self.robot_status["fan_status"]
+        return None
 
     @property
     def mop_status(self):
-        """ Raw fan_status field string """
-        if 'water_level' in self.robot_status:
-            return self.robot_status['water_level']
-    
+        """Raw fan_status field string"""
+        if "water_level" in self.robot_status:
+            return self.robot_status["water_level"]
+        return None
+
     @property
     def fan_speed_list(self):
-        """ Return Fan speed list available"""
+        """Return Fan speed list available"""
         return [self.FAN_SPEED_QUIET, self.FAN_SPEED_NORMAL, self.FAN_SPEED_HIGH]
 
     @property
     def mop_level_list(self):
-        """ Return Mop level list available"""
+        """Return Mop level list available"""
         return [self.MOP_SPEED_LOW, self.MOP_SPEED_NORMAL, self.MOP_SPEED_HIGH]
 
     @property
     def clean_time(self):
         """Return clean time"""
-        if 'clean_time' in self.robot_status:
-            return self.robot_status['clean_time']
+        if "clean_time" in self.robot_status:
+            return self.robot_status["clean_time"]
+        return 0
 
     @property
     def clean_area(self):
         """Return clean area in square meter"""
-        if 'clean_area' in self.robot_status:
-            return self.robot_status['clean_area']
+        if "clean_area" in self.robot_status:
+            return self.robot_status["clean_area"]
+        return 0
 
     @property
     def vacuum_or_mop(self) -> int:
-        """ Find if the robot is in vacuum or mop mode """
-        if 'fan_status' and 'water_level' in self.robot_status:
-            if self.robot_status['fan_status'] == self.FAN_DISABLED and self.robot_status['water_level'] != self.MOP_DISABLED:
+        """Find if the robot is in vacuum or mop mode"""
+        if "fan_status" and "water_level" in self.robot_status:
+            if (
+                self.robot_status["fan_status"] == self.FAN_DISABLED
+                and self.robot_status["water_level"] != self.MOP_DISABLED
+            ):
                 return self.MOP_ON
             return self.VACUUM_ON
         return self.NO_FAN_NO_MOP
@@ -171,59 +205,69 @@ class VacDevice(WebackWssCtrl):
     # -> Method
 
     async def set_fan_water_speed(self, speed):
-        """ User for set both : fan speed and water level"""
+        """User for set both : fan speed and water level"""
         # Checking if robot is running otherwise it will not apply value
         if not self.is_cleaning:
-            _LOGGER.info(f"Vacuum: Can't set set fan/water speed (value={speed}) robot is not running.")
+            _LOGGER.info(
+                "Vacuum: Can't set set fan/water speed (value=%s) robot is not running.", speed
+            )
             return
         # Checking value are allowed
         if speed not in self.FAN_SPEEDS and speed not in self.MOP_SPEEDS:
-            _LOGGER.error(f"Error: Fan/Mop value={speed} is not available")
+            _LOGGER.error("Error: Fan/Mop value=%s is not available", speed)
             return
         # Sending command
         working_payload = {self.SET_FAN_SPEED: speed}
         await self.send_command(self.name, self.sub_type, working_payload)
         return
-    
+
     async def turn_on(self):
+        """ Turn ON vacuum """
         working_payload = {self.ASK_STATUS: self.CLEAN_MODE_AUTO}
         await self.send_command(self.name, self.sub_type, working_payload)
         return
 
     async def turn_off(self):
+        """ Turn OFF vacuum """
         working_payload = {self.ASK_STATUS: self.CHARGE_MODE_RETURNING}
         await self.send_command(self.name, self.sub_type, working_payload)
         return
-    
+
     async def pause(self):
+        """ Pause vacuum """
         working_payload = {self.ASK_STATUS: self.CLEAN_MODE_STOP}
         await self.send_command(self.name, self.sub_type, working_payload)
         return
-    
+
     async def clean_spot(self):
+        """ Clean spot command """
         working_payload = {self.ASK_STATUS: self.CLEAN_MODE_SPOT}
         await self.send_command(self.name, self.sub_type, working_payload)
         return
-    
+
     async def locate(self):
+        """ Locate vacuum """
         working_payload = {self.ASK_STATUS: self.ROBOT_LOCATION_SOUND}
         await self.send_command(self.name, self.sub_type, working_payload)
         return
-    
+
     async def return_to_base(self):
+        """ Return to base command """
         working_payload = {self.ASK_STATUS: self.CHARGE_MODE_RETURNING}
         await self.send_command(self.name, self.sub_type, working_payload)
         return
 
     async def goto(self, point: str):
+        """ GoTo point command """
         working_payload = {
             self.ASK_STATUS: self.ROBOT_PLANNING_LOCATION,
             self.GOTO_POINT: point,
         }
         await self.send_command(self.name, self.sub_type, working_payload)
         return
-    
+
     async def clean_rect(self, rectangle: str):
+        """ Clean rectangle command """
         working_payload = {
             self.ASK_STATUS: self.ROBOT_PLANNING_RECT,
             self.RECTANGLE_INFO: rectangle,
@@ -232,43 +276,55 @@ class VacDevice(WebackWssCtrl):
         return
 
     async def voice_mode(self, state: str):
+        """ Voice mode """
         if state in self.SWITCH_VALUES:
             working_payload = {self.VOICE_SWITCH: state}
             await self.send_command(self.name, self.sub_type, working_payload)
         else:
-            _LOGGER.error(f"Voice mode can't be set with value : {state}")
+            _LOGGER.error("Voice mode can't be set with value : %s ", state)
         return
 
     async def undisturb_mode(self, state: str):
+        """ Undisturb mode """
         if state in self.SWITCH_VALUES:
             working_payload = {self.UNDISTURB_MODE: state}
             await self.send_command(self.name, self.sub_type, working_payload)
         else:
-            _LOGGER.error(f"Undisturb mode can't be set with value : {state}")
+            _LOGGER.error("Undisturb mode can't be set with value : %s ", state)
         return
 
-    async def clean_room(self, room_ids: list):
-        room_data = list()
-        for id in room_ids:
-            room_data.append(dict(room_id = id))
-        working_payload = {self.ASK_STATUS: self.CLEAN_MODE_ROOMS, self.SELECTED_ZONE: room_data}
+    async def clean_room(self, room_ids_list: list):
+        """ Clean room command """
+        room_data = []
+        for room_id in room_ids_list:
+            room_data.append(dict(room_id=room_id))
+        working_payload = {
+            self.ASK_STATUS: self.CLEAN_MODE_ROOMS,
+            self.SELECTED_ZONE: room_data,
+        }
         await self.send_command(self.name, self.sub_type, working_payload)
 
     async def clean_zone(self, bounding):
+        """ Clean zone command """
         box_x = []
         box_y = []
         num_boxes = len(bounding)
 
         for box in bounding:
-            box_x.append(int(box[0]  / 10))
-            box_x.append(int(box[0]  / 10))
-            box_x.append(int(box[2]  / 10))
-            box_x.append(int(box[2]  / 10))
-            box_y.append(int(box[1]  / 10))
-            box_y.append(int(box[3]  / 10))
-            box_y.append(int(box[3]  / 10))
-            box_y.append(int(box[1]  / 10))
-        
-        working_payload = {self.ASK_STATUS: self.ROBOT_PLANNING_RECT, self.PLANNING_RECT_POINT_NUM: num_boxes * 4, self.PLANNING_RECT_X: box_x, self.PLANNING_RECT_Y: box_y}
-        _LOGGER.debug(f"Vacuum : Planning rect sent {working_payload}")
+            box_x.append(int(box[0] / 10))
+            box_x.append(int(box[0] / 10))
+            box_x.append(int(box[2] / 10))
+            box_x.append(int(box[2] / 10))
+            box_y.append(int(box[1] / 10))
+            box_y.append(int(box[3] / 10))
+            box_y.append(int(box[3] / 10))
+            box_y.append(int(box[1] / 10))
+
+        working_payload = {
+            self.ASK_STATUS: self.ROBOT_PLANNING_RECT,
+            self.PLANNING_RECT_POINT_NUM: num_boxes * 4,
+            self.PLANNING_RECT_X: box_x,
+            self.PLANNING_RECT_Y: box_y,
+        }
+        _LOGGER.debug("Vacuum : Planning rect sent %s", working_payload)
         await self.send_command(self.name, self.sub_type, working_payload)
